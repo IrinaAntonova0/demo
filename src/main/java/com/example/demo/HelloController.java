904b427c26b7859a5dfaaa9a1c6e29c2d4b752b8
IrinaAntonova0 <irina.antonova.dipl.inf@gmail.com>
2026-05-21 12:34:23 +0200

    Initial MessageController mit MessageListe und füllendem MessagePost

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,24 +2,38 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("/api")
 public class HelloController {
 
-/*    Student student = new Student("x", 0);*/
+    List<Message> messageList = new ArrayList<>();
 
-    @GetMapping
+    @GetMapping("/hello")
     public String sayHello(){
         return "Hi there.";
     }
 
-    @GetMapping("/{greeting}")//localhost:8080/api/hello/Liebe?name=iri
+    @GetMapping("/hello/{greeting}")//localhost:8080/api/hello/Liebe?name=iri
     public String sayHello2(@PathVariable String greeting,
                             @RequestParam(required = false) String name)
     {
         return greeting + " " + name;
     }
 
+    @GetMapping("/messages/all")//local:8080/api/hello/2?q=test
+    public String getMessages()
+    {
+        return this.messageList.toString();
+    }
 
+    @PostMapping("/messages")
+    public String postStudent(@RequestBody Message message)
+    {
+        this.messageList.add(message);
+        return this.messageList.getLast().toString();
+    }
 }
 
---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class Message {
+    String name;
+    String message;
+    String id;
+}
